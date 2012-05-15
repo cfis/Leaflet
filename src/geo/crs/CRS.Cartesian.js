afb152fa381a5d2ef6d7bc464f29214082d6a801
Charlie Savage <cfis@zerista.com>
2012-05-14 21:05:07 -0600

    Use this.options and some source formatting.

--- src/geo/crs/CRS.Cartesian.js
+++ src/geo/crs/CRS.Cartesian.js
@@ -15,14 +15,14 @@ L.CRS.Cartesian = L.Class.extend({
         var boundsWidth = bounds.max.x - bounds.min.x;
         var boundsHeight = bounds.max.y - bounds.min.y;
 
-        var xOffset = options.falseEasting || 0;
+        var xOffset = this.options.falseEasting || 0;
         var xOffsetPercentage = 0 + xOffset / (boundsWidth * 1.0)
 
-        var yOffset = options.falseNorthing || 0;
+        var yOffset = this.options.falseNorthing || 0;
         var yOffsetPercentage = 1 - yOffset / (boundsHeight * 1.0)
 
         this.projection = L.Projection.Identity;
         this.transformation = new L.Transformation(1.0/boundsWidth, xOffsetPercentage,
-            -1.0/boundsHeight, yOffsetPercentage);
+                                                   -1.0/boundsHeight, yOffsetPercentage);
     }
 });
